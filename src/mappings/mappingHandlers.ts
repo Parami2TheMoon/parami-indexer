a9ddd6acc5df7c6ec6a4b02b577ba4e48eac8913
chentn <chentn@fenbi.com>
2021-11-19 12:03:07 +0800

    ADD: more log for sure

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,28 +3,19 @@ import {Balance} from "@polkadot/types/interfaces";
 import { Did } from "../types/models/Did";
 
 
-// export async function handleBlock(block: SubstrateBlock): Promise<void> {
-//     //Create a new starterEntity with ID using block hash
-//     let record = new StarterEntity(block.block.header.hash.toString());
-//     //Record block number
-//     record.field1 = block.block.header.number.toNumber();
-//     await record.save();
-// }
+export async function handleBlock(block: SubstrateBlock): Promise<void> {
+    logger.info("mappingHandler got a block: ", block.block.header.number.toNumber());
+}
 
 export async function handleDidAssigned(event: SubstrateEvent): Promise<void> {
     // const {event: {data: [account, balance]}} = event;
     // const record = await Did.get(event['Di']);
     // await record.save();
-    console.log("DidAssigned event got: ", event);  
+    logger.info("mappingHandler got aDidAssigned event: ", event);  
 }
 
-// export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-//     const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-//     //Date type timestamp
-//     record.field4 = extrinsic.block.timestamp;
-//     //Boolean tyep
-//     record.field5 = true;
-//     await record.save();
-// }
+export async function handleTimestampSet(extrinsic: SubstrateExtrinsic): Promise<void> {
+    logger.info("mappingHandler got timestamp set call: ", extrinsic);  
+}
 
 
