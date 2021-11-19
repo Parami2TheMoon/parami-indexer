52afe332db18ccda0bb6a3ff0211885912ceb3b4
chentn <chentn@fenbi.com>
2021-11-19 15:50:40 +0800

    ADD: presentation for save&query did from parami substrate chain

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -6,12 +6,15 @@ import { Did } from "../types/models/Did";
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
     logger.debug("mappingHandler got a block: ", block.block.header.number.toNumber());
 }
-
+/**
+ * //{"phase":{"ApplyExtrinsic":"1"},"event":{"method":"Assigned","section":"did","index":"0x2600","data":["0x12816265b2a84ce366f674995e5c439365712c55","5EYCAe5ijQkVKBy52wmGD3kJnFQ8CmyVD9U2ecNMQFoepQvb",null]},"topics":[]}
+ * @param event 
+ */
 export async function handleDidAssigned(event: SubstrateEvent): Promise<void> {
-    // const {event: {data: [account, balance]}} = event;
-    // const record = await Did.get(event['Di']);
-    // await record.save();
-    logger.info("mappingHandler got aDidAssigned event: ", event);  
+    logger.info(`mappingHandler got aDidAssigned event: ${JSON.stringify(event.toHuman())}` );  
+    const {event: {data: [did, stashAccount]}} = event;
+    const record = new Did(did.toHuman() as string);
+    await record.save();
 }
 
 export async function handleTimestampSet(extrinsic: SubstrateExtrinsic): Promise<void> {
