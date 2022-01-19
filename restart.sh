cc54f9b46bc3c01f9f59f6f93fda3d8e81a015b2
RightSun <szz@joymoe.com>
2022-01-19 23:31:11 +0800

    yarn codegen

--- restart.sh
+++ restart.sh
@@ -2,6 +2,7 @@
 systemctl stop subql-indexer.service
 systemctl stop subql-query.service
 rm -rf dist
+yarn codegen
 yarn build
 sudo -u postgres psql -f ./clean.sql
 systemctl start subql-indexer.service
