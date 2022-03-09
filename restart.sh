d6960d06a5a2658cc61cc1f281abb7a4c5a0ec35
OctopusR <Right.Sun@outlook.com>
2022-03-09 16:19:01 +0800

    ADD chown to restart script

--- restart.sh
+++ restart.sh
@@ -4,6 +4,7 @@ systemctl stop subql-query.service
 rm -rf dist
 yarn codegen
 yarn build
+chown -R www-data:www-data /var/www/parami-scanner
 sudo -u postgres psql -f ./clean.sql
 systemctl start subql-indexer.service
 systemctl start subql-query.service
